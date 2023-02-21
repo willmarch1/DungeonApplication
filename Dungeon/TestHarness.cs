e22473933e131ecd99de2bd9138c7d21b73b4060
Will March <willmarch11@hotmail.com>
2023-02-21 14:25:26 -0600

    wireframed weapon and character classes

---
+++ Dungeon/TestHarness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            //Build and test the functionality of our library
+            //Build and test a weapon
+            //build and test a character - include CalcBlock(), CalcHitChance(), CalcDamage()
+        }
+    }
+}
---
+++ DungeonLibrary/Character.cs
@@ -0,0 +1,27 @@
+﻿namespace DungeonLibrary
+{
+    public class Character
+    {
+        //Fields
+
+        /*
+         * int life (cannot be more than maxlife
+         * int maxlife (assign first in ctor)
+         * string name 
+         * int hitchance
+         * int block
+         * 
+         */
+
+        //Properties - 1 for each field 
+
+        //Ctors - 1 fully qualified, 1 default/unqualified
+
+        //Methods
+        //ToString() override
+
+        //CalcBlock() returns an int -> return Block;
+        //CalcHitChance() returns an int -> return HitChance;
+        //CalcDamage() returns an int -> return 0;
+    }
+}
